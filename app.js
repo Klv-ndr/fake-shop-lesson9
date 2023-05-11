47c4b3e3422004b0ea0b41b51028fdef726ad10e
Klv-ndr <128741871+Klv-ndr@users.noreply.github.com>
2023-05-11 10:55:39 +0300

    create slider

--- app.js
+++ app.js
@@ -19,3 +19,47 @@ addToCartBtns.forEach((item) => {
     productsCountEL.textContent = +productsCountEL.textContent + 1;
   });
 });
+
+// change like state
+
+let likeBtns = document.querySelectorAll(".like");
+console.log(likeBtns);
+
+likeBtns.forEach((item) => {
+  item.addEventListener("click", function () {
+    item.classList.toggle("liked");
+  });
+});
+
+//more details
+
+let moreDetailsBtns = document.querySelectorAll(".details");
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".btn-close");
+
+moreDetailsBtns.forEach((item) => {
+  item.addEventListener("click", openModal);
+});
+
+closeBtn.addEventListener("click", closeModal);
+
+function openModal() {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+}
+
+function closeModal() {
+  modal.classList.remove("show");
+  modal.classList.add("hide");
+}
+
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
+
+// slick
+$(".slider").slick({
+  dots: true,
+});
